4d87542efbbbd8e61fd2444b0b7e9cc8fa9bd412
Callum Dunster <cdunster@users.noreply.github.com>
2022-02-10 22:47:01 +0100

    Add git config to home-manager.

--- home.nix
+++ home.nix
@@ -6,7 +6,6 @@
     gnomeExtensions.material-shell
     dconf2nix
     starship
-    git
     lazygit
     bat
     exa
@@ -28,4 +27,26 @@
       lg = "lazygit";
     };
   };
+
+  programs.git = {
+    enable = true;
+    userName = "Callum Dunster";
+    extraConfig = {
+      credential.helper = "store";
+      init.defaultBranch = "main";
+      pull.ff = "only";
+      "includeIf \"gitdir:~/repos/work/\"".path = "~/repos/work/.gitconfig";
+      "includeIf \"gitdir:~/repos/personal/\"".path = "~/repos/personal/.gitconfig";
+    };
+  };
+
+  home.file."repos/work/.gitconfig".text = ''
+    [user]
+        email = "callum.dunster@evbox.com";
+  '';
+
+  home.file."repos/personal/.gitconfig".text = ''
+    [user]
+        email = "cdunster@users.noreply.github.com";
+  '';
 }
