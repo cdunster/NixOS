0925b8edbc4a2b9d8a434d3fe8ff8fae7be499b9
Callum Dunster <cdunster@users.noreply.github.com>
2022-03-17 15:22:19 +0100

    fish: Bind C-p to start tmuxinator project.

--- home.nix
+++ home.nix
@@ -57,11 +57,28 @@
           commandline --function repaint
         '';
       };
+      _fzf_start_tmuxinator = {
+        body = ''
+          set _projects (tmuxinator list --newline)
+          set _result (printf '%s\n' $_projects | fzf-tmux -p --header-lines=1 --print-query)
+          set _query $_result[1]
+          set _selected $_result[2]
+
+          if contains "$_selected" $_projects
+              tmuxinator start $_selected
+          else if test -n "$_query"
+              tmuxinator new $_query
+          end
+
+          commandline --function repaint
+        '';
+      };
     };
     shellInit = ''
       set -g fish_greeting ""
 
       bind \cf "_fzf_open_file_nvim"
+      bind \cp "_fzf_start_tmuxinator"
     '';
   };
 
