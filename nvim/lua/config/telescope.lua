b2eee640f7489191952f06ad0b21fea617861852
Callum Dunster <cdunster@users.noreply.github.com>
2022-10-12 21:34:52 +0200

    nvim: Change bindings to not conflict with Zellij.

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -18,13 +18,10 @@ M.config = function()
     local wk = require("which-key")
 
     wk.register({
-        ["<C-f>"] = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find a file in working dir" },
-        ["<C-g>"] = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "Search file content in working dir" },
-        ["<C-h>"] = { "<cmd>lua require('telescope.builtin').grep_string()<cr>", "Search current string in working dir" },
-        ["<C-b>"] = { "<cmd>lua require('telescope.builtin').buffers()<cr>", "List open buffers" },
-    })
-
-    wk.register({
+        ["f"] = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find a file in working dir" },
+        ["/"] = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "Search file content in working dir" },
+        ["*"] = { "<cmd>lua require('telescope.builtin').grep_string()<cr>", "Search current string in working dir" },
+        ["b"] = { "<cmd>lua require('telescope.builtin').buffers()<cr>", "List open buffers" },
         ["<leader>"] = { "<cmd>lua require('telescope.builtin').resume()<cr>", "Resume last telescope picker" },
     }, { prefix = "<leader>" })
 end
