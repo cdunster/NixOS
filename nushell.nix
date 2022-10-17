6b2c592382a88dfeb99bcde2eeaa2d69ac54a0c3
Callum Dunster <cdunster@users.noreply.github.com>
2022-10-17 10:35:38 +0200

    nushell: Add direnv support.

--- nushell.nix
+++ nushell.nix
@@ -7,7 +7,16 @@
     '';
     configFile.text = ''
       let-env config = {
-          show_banner: false
+        show_banner: false
+        hooks: {
+          pre_prompt: [{
+            code: "
+              let direnv = (direnv export json | from json)
+              let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
+              $direnv | load-env
+            "
+          }]
+        }
       }
       source ~/.cache/starship/init.nu
     '';
