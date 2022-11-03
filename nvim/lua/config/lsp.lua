10cb0ffe215d3d28daff20c2bdaab6ab040489a1
Callum Dunster <cdunster@users.noreply.github.com>
2022-11-03 13:25:49 +0100

    nvim: Remove proto files from clangd LSP.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -52,6 +52,7 @@ M.config = function()
     lsp.clangd.setup({
         on_attach = on_attach,
         capabilities = capabilities,
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
     })
 
     -- Use rust-tools plugin for Rust.
