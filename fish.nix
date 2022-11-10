978f006db6a5ea5f73e674d7f21b07805ce1adeb
Callum Dunster <cdunster@users.noreply.github.com>
2022-11-10 22:09:38 +0100

    fish: Remove tmux specific fish functions.

--- fish.nix
+++ fish.nix
@@ -23,25 +23,9 @@
     functions = {
       _fzf_open_file_nvim = {
         body = ''
-          set _selected (fzf-tmux -p 90%,90% --multi --preview="_fzf_preview_file {}" --header="Open in nvim:")
+          set _selected (fzf --multi --preview="_fzf_preview_file {}" --header="Open in nvim:")
           if test -n "$_selected"
               nvim $_selected
-          end
-
-          commandline --function repaint
-        '';
-      };
-      _fzf_start_tmuxinator = {
-        body = ''
-          set _projects (tmuxinator list --newline)
-          set _result (printf '%s\n' $_projects | fzf-tmux -p 40%,30% --header-lines=1 --print-query)
-          set _query $_result[1]
-          set _selected $_result[2]
-
-          if contains "$_selected" $_projects
-              tmuxinator start $_selected
-          else if test -n "$_query"
-              tmuxinator new $_query
           end
 
           commandline --function repaint
@@ -52,7 +36,7 @@
           set _rg_cmd "rg --column --line-number --no-heading --color=always --smart-case"
           set _query (commandline --current-token)
           set _fzf (FZF_DEFAULT_COMMAND="$_rg_cmd '$_query'" \
-              fzf-tmux -p 90%,90% \
+              fzf \
               --ansi \
               --disabled \
               --query $_query \
@@ -68,23 +52,6 @@
           if test -n "$_fzf[1]"
               nvim +$_fzf[2] $_fzf[1]
           end
-        '';
-      };
-      _fzf_tmux_sessions = {
-        body = ''
-          set _sessions (tmux ls 2> /dev/null)
-          set _result (printf '%s\n' $_sessions | fzf-tmux -p 40%,30% --print-query)
-          set _query $_result[1]
-          set _selected $_result[2]
-
-          if contains "$_selected" $_sessions
-              set _details (string split ':' $_selected)
-              tmux new-session -A -s $_details[1]
-          else if test -n "$_query"
-              tmux new-session -A -s $_query
-          end
-
-          commandline --function repaint
         '';
       };
       lf = {
@@ -105,9 +72,7 @@
       set -g fish_greeting ""
 
       bind \cf _fzf_open_file_nvim
-      bind \cp _fzf_start_tmuxinator
       bind \cg _fzf_live_grep
-      bind \cs _fzf_tmux_sessions
     '';
   };
 
