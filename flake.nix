d8490a441c24f4e72156e39120e0bf5d8fb9eb72
Callum Dunster <cdunster@users.noreply.github.com>
2023-01-10 13:28:28 +0100

    flake: Move overlay to new file.

--- flake.nix
+++ flake.nix
@@ -24,51 +24,7 @@
         config.allowUnfree = true;
         overlays = [
           nixgl.overlay
-          (final: prev: {
-            zellij = with pkgs; rustPlatform.buildRustPackage
-              rec {
-                pname = "zellij";
-                version = "0.31.4";
-
-                src = fetchFromGitHub {
-                  owner = "zellij-org";
-                  repo = "zellij";
-                  rev = "v${version}";
-                  sha256 = "sha256-eULdBwQNquk4jj1SjdMsAN7S7mBSZs7jVAwMyFvOlWk=";
-                };
-
-                cargoSha256 = "sha256-bKOY1r5SxAI9D+9YkYhX2l+pm2kZ6GEU2cf5NFqhuSU=";
-
-                nativeBuildInputs = [
-                  mandown
-                  installShellFiles
-                  pkg-config
-                ];
-
-                buildInputs = [
-                  openssl
-                ] ++ lib.optionals stdenv.isDarwin [
-                  libiconv
-                  DiskArbitration
-                  Foundation
-                ];
-
-                preCheck = ''
-                  HOME=$TMPDIR
-                '';
-
-                postInstall = ''
-                  mandown docs/MANPAGE.md > zellij.1
-                  installManPage zellij.1
-                  installShellCompletion --cmd $pname \
-                    --bash <($out/bin/zellij setup --generate-completion bash) \
-                    --fish <($out/bin/zellij setup --generate-completion fish) \
-                    --zsh <($out/bin/zellij setup --generate-completion zsh)
-                '';
-
-                passthru.tests.version = testers.testVersion { package = zellij; };
-              };
-          })
+          (import ./pkgs-override.nix)
         ];
       };
     in
---
+++ pkgs-override.nix
@@ -0,0 +1,47 @@
+final: prev: {
+  # This override is needed because:
+  # 1. From v0.32.0 the config format was changed to KDL which is not yet supported by Nix/home-manager.
+  # 2. Recent version seem to lag on startup querying for colour and pixel size.
+  zellij = with final; rustPlatform.buildRustPackage rec {
+    pname = "zellij";
+    version = "0.31.4";
+
+    src = fetchFromGitHub {
+      owner = "zellij-org";
+      repo = "zellij";
+      rev = "v${version}";
+      sha256 = "sha256-eULdBwQNquk4jj1SjdMsAN7S7mBSZs7jVAwMyFvOlWk=";
+    };
+
+    cargoSha256 = "sha256-bKOY1r5SxAI9D+9YkYhX2l+pm2kZ6GEU2cf5NFqhuSU=";
+
+    nativeBuildInputs = [
+      mandown
+      installShellFiles
+      pkg-config
+    ];
+
+    buildInputs = [
+      openssl
+    ] ++ lib.optionals stdenv.isDarwin [
+      libiconv
+      DiskArbitration
+      Foundation
+    ];
+
+    preCheck = ''
+      HOME=$TMPDIR
+    '';
+
+    postInstall = ''
+      mandown docs/MANPAGE.md > zellij.1
+      installManPage zellij.1
+      installShellCompletion --cmd $pname \
+        --bash <($out/bin/zellij setup --generate-completion bash) \
+        --fish <($out/bin/zellij setup --generate-completion fish) \
+        --zsh <($out/bin/zellij setup --generate-completion zsh)
+    '';
+
+    passthru.tests.version = testers.testVersion { package = zellij; };
+  };
+}
