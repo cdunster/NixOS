5950058424f03c7814f59af9f96453d0e623b3fb
Callum Dunster <cdunster@users.noreply.github.com>
2023-01-16 13:04:55 +0100

    kitty: Add font_features to style FiraCode font.

--- kitty.nix
+++ kitty.nix
@@ -8,6 +8,10 @@
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
+      # For the FiraCode font features, see: https://github.com/tonsky/FiraCode/wiki/How-to-enable-stylistic-sets
+      # This will stylise (in order) 'a' 'g' 'i' 'r' '@' '&' '<= >=' '\\' '=~ !~'
+      "font_features FiraCodeNerdFontComplete-Regular" = "+cv01 +cv02 +cv06 +ss01 +ss05 +ss03 +ss02 +ss06 +ss07";
+      "font_features FiraCodeNerdFontComplete-Bold" = "+cv01 +cv02 +cv06 +ss01 +ss05 +ss03 +ss02 +ss06 +ss07";
     };
   };
 }
