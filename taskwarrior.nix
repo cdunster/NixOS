e71fd759c6f0aa67c084ee0c710ca615d6038613
Callum Dunster <cdunster@users.noreply.github.com>
2023-01-26 09:43:57 +0100

    tmux: Remove taskwarrior popup binding.

--- taskwarrior.nix
+++ taskwarrior.nix
@@ -5,9 +5,5 @@
 
   programs.fish.shellAliases.tw = "taskwarrior-tui";
 
-  programs.tmux.extraConfig = ''
-    bind t popup -E -h 90% -w 90% "taskwarrior-tui"
-  '';
-
   programs.taskwarrior.enable = true;
 }
