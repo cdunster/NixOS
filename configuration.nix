bca89c8bc0ff9ede54d838ee3df21a829d20cef9
Callum Dunster <cdunster@users.noreply.github.com>
2024-01-08 15:17:51 +0100

    nixos: add GRUB bootloader as commented-out option

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,11 @@
     '';
   };
 
+  # Use the GRUB bootloader.
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/sda";
+  # boot.loader.grub.useOSProber = true;
+
   # Use the EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
