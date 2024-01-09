a5c50cfcc5773d28dd6f9272db7058c936530f12
Callum Dunster <cdunster@users.noreply.github.com>
2024-01-09 12:30:02 +0100

    nixos: add Pop!_OS GNOME shell icons

--- nixos-home.nix
+++ nixos-home.nix
@@ -8,5 +8,6 @@
     # Gnome
     # gnomeExtensions.material-shell # Nice WM for non-Pop!_OS systems:
     gnomeExtensions.pop-shell # The GNOME Shell used in Pop!_OS.
+    pop-icon-theme # The icons used by Pop!_OS.
   ];
 }
