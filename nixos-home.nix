4e7391f8aecc0b2bb586fb35fec362561ead4419
Callum Dunster <cdunster@users.noreply.github.com>
2024-01-09 15:16:24 +0100

    nixos: add GNOME dash-to-dock extension

--- nixos-home.nix
+++ nixos-home.nix
@@ -8,6 +8,7 @@
     # Gnome
     # gnomeExtensions.material-shell # Nice WM for non-Pop!_OS systems:
     gnomeExtensions.pop-shell # The GNOME Shell used in Pop!_OS.
+    gnomeExtensions.dash-to-dock # Configure the GNOME dash/dock.
     gnome.gnome-session # Utility to control the GNOME shell session.
     pop-icon-theme # The icons used by Pop!_OS.
   ];
