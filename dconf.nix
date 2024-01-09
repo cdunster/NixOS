5014112801041517ef2fce094295df5ceb3dc6e6
Callum Dunster <cdunster@users.noreply.github.com>
2024-01-09 15:39:53 +0100

    dconf: fix touchpad scrolling

--- dconf.nix
+++ dconf.nix
@@ -262,6 +262,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = false;
       speed = 0.184;
       two-finger-scrolling-enabled = true;
     };
