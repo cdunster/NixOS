2ba139c2b56c9e0dbf852fed7589987ecf8f6f5c
Callum Dunster <cdunster@users.noreply.github.com>
2024-02-19 10:23:54 +0100

    dconf: enable tap-to-click for touchpad

--- dconf.nix
+++ dconf.nix
@@ -269,6 +269,7 @@ with lib.hm.gvariant;
     "org/gnome/desktop/peripherals/touchpad" = {
       natural-scroll = false;
       speed = 0.184;
+      tap-to-click = true;
       two-finger-scrolling-enabled = true;
     };
 
