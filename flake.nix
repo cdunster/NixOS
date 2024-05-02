f73b7ec9c03ef3ca5650133975be55ca9271877e
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-02 13:51:19 +0200

    flake: fix home-manager extraSpecialArgs

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.callum = import ./nixos-home.nix;
-            home-manager.extraSpecialArgs = inputs;
+            home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
