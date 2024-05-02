0245e93cd36cdf2914dd8d869fe28b4bab7ed767
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-02 14:42:21 +0200

    waybar: add initial support with tmux style

--- wayland.nix
+++ wayland.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     ./hyprland.nix # Use Hyprland, as the Wayland compositor.
+    ./waybar.nix # Use Waybar as the statusbar for Wayland.
   ];
 
   home.packages = with pkgs; [
