12603a4f3e0d1c047e64dbf12da31635b862af33
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-02 12:50:46 +0200

    nix: add hyprland cachix substituter

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,10 @@
 
   nix = {
     package = pkgs.nixUnstable;
+    settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
       keep-outputs = true
