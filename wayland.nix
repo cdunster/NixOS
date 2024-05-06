2530d68a3919ab3e109bcbb51148caff06ae480f
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-06 10:39:20 +0200

    kanshi: add initial support for monitor profiles

--- wayland.nix
+++ wayland.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hyprland.nix # Use Hyprland, as the Wayland compositor.
     ./waybar.nix # Use Waybar as the statusbar for Wayland.
+    ./kanshi.nix # Use Kanshi for dynamic control of monitors on Wayland.
   ];
 
   home.packages = with pkgs; [
@@ -9,5 +10,6 @@
     dolphin # File browser
     swaybg # Used for setting the background image
     grimblast # Used for screenshots in Hyprland
+    kanshi # Used to reload/switch monitor profiles
   ];
 }
