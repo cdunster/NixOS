f4485bc2cc4ccd146c706eaf09af0ee5bdeb8e5a
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-15 11:12:50 +0200

    nixos: rebuild script doesn't use a profile on main branch

--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -5,5 +5,10 @@ branch=`(git branch 2>/dev/null | sed -n '/^\* / { s|^\* ||; p; }')`
 revision=`git describe --always --dirty`
 export NIXOS_PROFILE="$branch"
 export NIXOS_LABEL_VERSION="$today+${last_commit// /_}.$revision"
-echo "rebuilding and switching to NixOS with profile $NIXOS_PROFILE and label: $NIXOS_LABEL_VERSION"
-sudo nixos-rebuild switch -p "$NIXOS_PROFILE"
+if [[ $NIXOS_PROFILE == "main" ]]; then
+    echo "rebuilding and switching to NixOS with no profile and version: $NIXOS_LABEL_VERSION"
+    sudo nixos-rebuild switch
+else
+    echo "rebuilding and switching to NixOS with profile $NIXOS_PROFILE and version: $NIXOS_LABEL_VERSION"
+    sudo nixos-rebuild switch -p "$NIXOS_PROFILE"
+fi
