583ffae2307a44183bf8cd764b410e77dedd18fd
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-21 10:59:24 +0200

    nixos: rebuild script just updates the profile
    The version nor the label didn't work so...

--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-today=`date +%Y.%m.%d`
-last_commit=`(git log -1 --pretty=%B | tr -dc '[:alnum:] & :_.-')`
+
 branch=`(git branch 2>/dev/null | sed -n '/^\* / { s|^\* ||; p; }')`
-revision=`git describe --always --dirty`
 export NIXOS_PROFILE="$branch"
-export NIXOS_LABEL_VERSION="$today+${last_commit// /_}.$revision"
 if [[ $NIXOS_PROFILE == "main" ]]; then
-    echo "rebuilding and switching to NixOS with no profile and version: $NIXOS_LABEL_VERSION"
+    echo "rebuilding NixOS and switching to it"
     sudo nixos-rebuild switch
 else
-    echo "rebuilding and switching to NixOS with profile $NIXOS_PROFILE and version: $NIXOS_LABEL_VERSION"
+    echo "rebuilding and switching to NixOS with profile $NIXOS_PROFILE"
     sudo nixos-rebuild switch -p "$NIXOS_PROFILE"
 fi
