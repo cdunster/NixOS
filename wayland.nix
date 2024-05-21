0045363ea063d5812f3394bef7ec8e1293007caf
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-21 12:10:56 +0200

    hypridle: add initial support

--- wayland.nix
+++ wayland.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     ./hyprland.nix # Use Hyprland, as the Wayland compositor.
+    ./hypridle.nix # Use Hypridle, as the idle daemon for Hyprland.
     ./hyprlock.nix # Use Hyprlock as the lockscreen for Hyprland.
     ./hyprpaper.nix # Use Hyprpaper as the background setter for Hyprland.
     ./waybar.nix # Use Waybar as the statusbar for Wayland.
