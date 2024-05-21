c3808dba5f3cb3ec5a51a84d938bfc68959db595
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-21 17:27:02 +0200

    ags: add notification popups

--- ags/config.js
+++ ags/config.js
@@ -1,10 +1,12 @@
 import { Bar } from "./system_bar.js"
 import { APP_LAUNCHER } from "./app_launcher.js"
+import { NotificationPopups } from "./notifications-popups.js"
 
 App.config({
     style: "./style.css",
     windows: [
         Bar(),
-        APP_LAUNCHER
+        APP_LAUNCHER,
+        NotificationPopups(),
     ],
 })
--- ags/system_bar.js
+++ ags/system_bar.js
@@ -1,5 +1,4 @@
 const hyprland = await Service.import("hyprland")
-const notifications = await Service.import("notifications")
 const mpris = await Service.import("mpris")
 const audio = await Service.import("audio")
 const battery = await Service.import("battery")
@@ -41,25 +40,6 @@ function Clock() {
     return Widget.Label({
         class_name: "clock",
         label: date.bind(),
-    })
-}
-
-
-// we don't need dunst or any other notification daemon
-// because the Notifications module is a notification daemon itself
-function Notification() {
-    const popups = notifications.bind("popups")
-    return Widget.Box({
-        class_name: "notification",
-        visible: popups.as(p => p.length > 0),
-        children: [
-            Widget.Icon({
-                icon: "preferences-system-notifications-symbolic",
-            }),
-            Widget.Label({
-                label: popups.as(p => p[0]?.summary || ""),
-            }),
-        ],
     })
 }
 
@@ -175,7 +155,6 @@ function Center() {
         spacing: 8,
         children: [
             Clock(),
-            Notification(),
         ],
     })
 }
