32a7221eaf656d64ee363a4b0c59c433328cd8e0
Callum Dunster <cdunster@users.noreply.github.com>
2024-05-26 19:28:34 +0200

    ags: sort workspaces numerically

--- ags/system_bar.js
+++ ags/system_bar.js
@@ -17,11 +17,14 @@ const date = Variable("", {
 function Workspaces() {
     const activeId = hyprland.active.workspace.bind("id")
     const workspaces = hyprland.bind("workspaces")
-        .as(ws => ws.map(({ id }) => Widget.Button({
-            on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-            child: Widget.Label(`${id}`),
-            class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
-        })))
+        .as(ws => ws
+            .sort((a, b) => a.id - b.id)
+            .map(({ id }) => Widget.Button({
+                on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+                child: Widget.Label(`${id}`),
+                class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+            }))
+        )
 
     return Widget.Box({
         class_name: "workspaces",
