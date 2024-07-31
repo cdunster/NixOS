32f7d514159cf7978500f08a0ad6aa4d7d363d3d
Callum Dunster <cdunster@users.noreply.github.com>
2024-07-31 14:22:16 +0200

    greetd: enable for hyprland configurations, use tuigreet for login

--- nixos-hyprland.nix
+++ nixos-hyprland.nix
@@ -1,9 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable the Hyprland Wayland compositor.
   programs.hyprland.enable = true;
 
   # Hint to electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Daemon for login management.
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 }
 
