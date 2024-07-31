cc51a8e77aa519f672b1bc73e4c6842c7af855fe
Callum Dunster <cdunster@users.noreply.github.com>
2024-07-31 13:49:06 +0200

    nix: remove unused 'home.packages'

--- nixos-home.nix
+++ nixos-home.nix
@@ -1,5 +1,5 @@
 # Manage user accounts with home-manager for NixOS systems.
-{ config, pkgs, ... }: {
+{ ... }: {
   imports = [
     ./home.nix # Import the standard settings for all systems.
     ./home-nixos-gnome.nix # Use Gnome with NixOS specific settings and packages.
@@ -11,7 +11,4 @@
 
   # For managing networks in the system tray.
   services.network-manager-applet.enable = true;
-
-  home.packages = with pkgs; [
-  ];
 }
