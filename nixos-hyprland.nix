bff2c24901537c7c39e6bed38e0fd7eecb130fe6
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-01 13:42:05 +0200

    nixos: add upower service for battery monitoring

--- nixos-hyprland.nix
+++ nixos-hyprland.nix
@@ -6,6 +6,9 @@
   # Hint to electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  # Daemon for monitoring the battery.
+  services.upower.enable = true;
+
   # Daemon for login management.
   services.greetd = {
     enable = true;
