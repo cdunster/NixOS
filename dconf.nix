3924959bd536b1d339239087f7a240a043a2868c
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-05 10:41:10 +0200

    dconf: disable touchpad whilst typing

--- dconf.nix
+++ dconf.nix
@@ -279,6 +279,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/peripherals/touchpad" = {
+      disable-while-typing = true;
       natural-scroll = false;
       speed = 0.184;
       tap-to-click = true;
