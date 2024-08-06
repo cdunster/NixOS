629d129a41a9d8c25b2d9176fb9b25f830207777
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-06 21:57:14 +0200

    nix: cleanup flake file

--- flake.nix
+++ flake.nix
@@ -15,66 +15,61 @@
     catppuccin.url = "github:catppuccin/nix"; # The catppuccin theme for everything
   };
 
-  outputs = { ... }@inputs:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.MiNixOS = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            (final: _prev: { cdunster = import inputs.cdunster-nixpkgs { system = final.system; config = final.config; }; })
-          ];
-        };
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hardware-configuration.nix
-          ./configuration.nix
-          ./nixos-gnome.nix
-          inputs.catppuccin.nixosModules.catppuccin
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.callum = {
-              imports = [
-                ./nixos-home.nix
-                inputs.catppuccin.homeManagerModules.catppuccin
-              ];
-            };
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-        ];
-      };
-
-      homeConfigurations.callum = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            inputs.nixgl.overlay
-            (import ./nixgl-wrapper.nix)
-            (import ./pkgs-override.nix)
-          ];
-        };
-        extraSpecialArgs = inputs;
-        modules = [
-          {
-            home = {
-              username = "callum";
-              homeDirectory = "/home/callum";
-            };
-          }
-          {
-            targets.genericLinux.enable = true;
-          }
-          ./home.nix
-          ./non-nixos-gnome.nix
-          inputs.catppuccin.homeManagerModules.catppuccin
-        ];
-      };
+  outputs = { ... }@inputs: {
+    nixosConfigurations.MiNixOS = inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        {
+          nixpkgs = {
+            config.allowUnfree = true;
+            overlays = [
+              (_final: prev: { cdunster = import inputs.cdunster-nixpkgs { system = prev.system; config = prev.config; }; })
+            ];
+          };
+        }
+        ./hardware-configuration.nix
+        ./configuration.nix
+        ./nixos-gnome.nix
+        inputs.catppuccin.nixosModules.catppuccin
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.callum = {
+            imports = [
+              ./nixos-home.nix
+              inputs.catppuccin.homeManagerModules.catppuccin
+            ];
+          };
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
     };
+
+    homeConfigurations.callum = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        config.allowUnfree = true;
+        overlays = [
+          inputs.nixgl.overlay
+          (import ./nixgl-wrapper.nix)
+          (import ./pkgs-override.nix)
+        ];
+      };
+      extraSpecialArgs = inputs;
+      modules = [
+        {
+          home = {
+            username = "callum";
+            homeDirectory = "/home/callum";
+          };
+        }
+        {
+          targets.genericLinux.enable = true;
+        }
+        ./home.nix
+        ./non-nixos-gnome.nix
+        inputs.catppuccin.homeManagerModules.catppuccin
+      ];
+    };
+  };
 }
