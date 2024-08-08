9652cfd842fe5a0ad634c5a62b8d2450e13b0220
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-08 16:03:26 +0200

    gnome: remove unused extensions

--- home-nixos-gnome.nix
+++ home-nixos-gnome.nix
@@ -7,8 +7,5 @@
   home.packages = with pkgs; [
     # gnomeExtensions.material-shell # Nice WM for non-Pop!_OS systems:
     gnomeExtensions.pop-shell # The GNOME Shell used in Pop!_OS.
-    gnomeExtensions.dash-to-dock # Configure the GNOME dash/dock.
-    gnome.gnome-session # Utility to control the GNOME shell session.
-    pop-icon-theme # The icons used by Pop!_OS.
   ];
 }
