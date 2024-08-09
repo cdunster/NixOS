46657b596e3022b40fa6602110f38e126490006a
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-09 10:12:01 +0200

    nixos: add extra locale settings

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,19 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
   console = {
     font = "Lat2-Terminus16";
   };
