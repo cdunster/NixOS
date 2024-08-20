0abf574d637a4fab71bee531c0e9e241e6f92701
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-20 21:33:52 +0200

    tmux: change directory and session modules icons on statusline

--- tmux.nix
+++ tmux.nix
@@ -24,6 +24,9 @@
       set -g @catppuccin_window_default_text "#W"
       set -g @catppuccin_window_current_text "#W"
 
+      set -g @catppuccin_directory_icon "󰉖"
+      set -g @catppuccin_session_icon ""
+
       set -g @catppuccin_status_left_separator " "
       set -g @catppuccin_status_right_separator ""
       set -g @catppuccin_status_connect_separator "no"
