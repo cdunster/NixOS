9691d08cdd71c1ee72a390d1fc6285609704c351
Callum Dunster <cdunster@users.noreply.github.com>
2024-08-29 08:17:53 +0200

    nixos: switch back to systemd-boot instead of grub

--- configuration.nix
+++ configuration.nix
@@ -18,12 +18,13 @@
   };
 
   # Use the GRUB bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "nodev";
+  # boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.efiSupport = true;
 
   # Use the EFI bootloader.
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
