7f2dfacc1f29a8943c2a0c0e4d992d4a42353dad
Callum Dunster <cdunster@users.noreply.github.com>
2024-09-03 19:11:41 +0200

    Revert "dconf: window focus follows cursor"
    This reverts commit 4b3432404b8ba235330e2305ca608627c4a4b6cb.

--- dconf.nix
+++ dconf.nix
@@ -117,8 +117,6 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/wm/preferences" = {
-      auto-raise = true;
-      focus-mode = "mouse";
       num-workspaces = 5;
       resize-with-right-button = true;
     };
