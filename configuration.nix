1942c762ed496555b97ef2a50cb0d42c7fdc9b5a
Callum Dunster <cdunster@users.noreply.github.com>
2024-09-07 11:19:28 +0200

    nix: add holochain-wind-tunnel cachix substituter

--- configuration.nix
+++ configuration.nix
@@ -5,9 +5,11 @@
     settings = {
       substituters = [
         "https://holochain-ci.cachix.org"
+        "https://holochain-wind-tunnel.cachix.org"
       ];
       trusted-public-keys = [
         "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+        "holochain-wind-tunnel.cachix.org-1:tnSm+7Y3hDKOc9xLdoVMuInMA2AQ0R/99Ucz5edYGJw="
       ];
     };
     extraOptions = ''
