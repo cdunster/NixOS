96c18342976160f47891165b3f3a9e194e916256
Callum Dunster <cdunster@users.noreply.github.com>
2024-09-10 12:06:28 +0200

    nvim: add bindings for opening quickfix list with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,6 +21,8 @@ M.config = function()
         { "<leader>/",        require('telescope.builtin').live_grep,          desc = "Search file content in working dir" },
         { "<leader>*",        require('telescope.builtin').grep_string,        desc = "Search current string in working dir" },
         { "<leader>b",        require('telescope.builtin').buffers,            desc = "List open buffers" },
+        { "<leader>q",        require('telescope.builtin').quickfix,           desc = "List quick-fixes in telescope" },
+        { "<leader>Q",        require('telescope.builtin').quickfixhistory,    desc = "List quick-fix history in telescope" },
         { "<leader><leader>", require('telescope.builtin').resume,             desc = "Resume last telescope picker" },
 
         { ":",                require('telescope').extensions.cmdline.cmdline, desc = "Open the cmdline in a telescope window" },
