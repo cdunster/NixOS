e20daa8a36e63bc108d7303a0ad88aea118bf2c4
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-08 22:01:12 +0200

    wezterm: start mux-server on GNOME login

--- wezterm.nix
+++ wezterm.nix
@@ -3,4 +3,11 @@
     enable = true;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
+
+  xdg.configFile."autostart/wezterm-mux-server.desktop".text = (pkgs.makeDesktopItem {
+    name = "wezterm-mux-server";
+    desktopName = "WezTerm mux server";
+    exec = "${pkgs.wezterm}/bin/wezterm-mux-server --daemonize";
+    noDisplay = true;
+  }).text;
 }
