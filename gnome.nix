48c8e8ff26efdc1d6396a7e7f884f1b409a271ba
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-08 21:14:13 +0200

    gnome: add comment about tray icon extension

--- gnome.nix
+++ gnome.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [
     gnome-extension-manager # Find, install, and manager gnome extensions
     gnome-tweaks # Extra settings for tweaking gnome
-    gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.tray-icons-reloaded # Show notification icons in the system tray/bar
   ];
 }
