5dacf8238db8f9fda48531844078f72fe22d530d
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-08 21:14:58 +0200

    gnome: add system monitoring extension

--- gnome.nix
+++ gnome.nix
@@ -4,5 +4,6 @@
     gnome-extension-manager # Find, install, and manager gnome extensions
     gnome-tweaks # Extra settings for tweaking gnome
     gnomeExtensions.tray-icons-reloaded # Show notification icons in the system tray/bar
+    gnomeExtensions.system-monitor # Show system stats in the system tray/bar
   ];
 }
