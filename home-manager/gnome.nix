8840f4288c9c74d93356e006a6984100a6a7e610
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-17 22:21:39 +0200

    nix: combine NixOS-specific GNOME stuff with general
    No longer supporting non-NixOS.

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -1,4 +1,4 @@
-# The general Gnome utilities, plugins, and settings for both NixOS and non-NixOS systems.
+# GNOME utilities, plugins, and settings
 { pkgs, ... }: {
   home.packages = with pkgs; [
     gnome-extension-manager # Find, install, and manager gnome extensions
@@ -8,6 +8,7 @@
   programs.gnome-shell = {
     enable = true;
     extensions = with pkgs.gnomeExtensions; [
+      { package = pop-shell; } # The GNOME Shell extension used in Pop!_OS for tiling WM
       { package = tray-icons-reloaded; } # Show notification icons in the system tray/bar
       { package = system-monitor; } # Show system stats in the system tray/bar
       { package = freon; } # System stats such as temps
