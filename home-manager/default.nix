05a91e3974c4518d6efac108f34210db2959e325
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-27 15:03:17 +0100

    nix: move home-manager config into single file

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,14 +1,134 @@
 # This is actually the NixOS module used to enable and setup home-manager
-{ inputs, config, ... }: {
+{ inputs, pkgs, config, ... }:
+let
+  user = config.hostOptions.user;
+in
+{
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.${config.hostOptions.user} = ./home.nix;
-        backupFileExtension = "bak";
-      };
-    }
   ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "bak";
+  };
+
+  home-manager.users.${user} = {
+    # Let home-manager manage its own installation.
+    programs.home-manager.enable = true;
+
+    # Add the background image.
+    home.file.".local/share/backgrounds/".source = ./images/backgrounds;
+
+    # Add user profile picture.
+    home.file.".face".source = ./images/backgrounds/deer_sunset.jpg;
+
+    # The version of Home-manager used.
+    home.stateVersion = "22.11";
+
+    home.packages = with pkgs; [
+      # Utilities
+      clang # Modern C/C++ compiler
+      xclip # CLI clipboard manager
+      fd # Find files and directories
+      ripgrep # Search content of files
+      ranger # Explore directories and files in the terminal
+      grex # Get regex from results
+      rargs # Batch CLI stuff (xargs + awk)
+      slides # Slideshow in the terminal using Markdown
+      glow # Render Markdown in the CLI
+      asciidoctor-with-extensions # Convert asciidoc files into other formats
+      pandoc # Powerful document conversion tool
+      plantuml # Create UML diagrams with a markup language
+      nodePackages.mermaid-cli # Create nicer looking diagrams with a markup language
+      xxd # Hex dumper
+      usbutils # lsusb and other utilities
+      vlc # VLC media player
+      zoom-us # Video conferencing software
+
+      # Fonts
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      cdunster.ttf-wps-fonts
+
+      # Nix tools
+      nixpkgs-fmt # Format Nix files the nixpkgs' way
+
+      # Applications
+      spotify # Proprietary music streaming
+      gimp # Edit photos for freeeeeee
+      anki-bin # Free spaced repetition learning tool
+      godot_4 # Create games for freeeeeee
+      wpsoffice # A free office suit
+      obs-studio # Video/Screen recording and streaming app
+      discord # Chat desktop app for gamers and communities
+      ledger-live-desktop # Desktop app to control and update Ledger Nano X HW wallet
+      inkscape # Vector graphics for freeeeeee
+
+      # LSPs
+      clang-tools # C/C++
+      pyright # Python
+      lua-language-server # Lua
+      vscode-langservers-extracted # HTML/CSS/JSON/ESLint
+      rust-analyzer # Rust
+      nixd # Nix
+      buf-language-server # Protobuf
+
+      # DAPs Debuggers
+      lldb # The debugger for the LLVM Project (lldb-dap)
+      llvm # Compiler tools for the LLVM Project (llvm-symbolizer)
+
+      # Work
+      mattermost-desktop # Desktop client Mattermost; a collaboration and chat app for businesses
+    ];
+
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "JetBrainsMono Nerd Font 10" ];
+        monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
+      };
+    };
+
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        rust-lang.rust-analyzer
+        vadimcn.vscode-lldb
+      ];
+    };
+
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        notARepository = "skip";
+        gui = {
+          showCommandLog = false;
+        };
+      };
+    };
+
+    programs.fzf = {
+      enable = true;
+      defaultCommand = "fd --type=f --hidden --exclude=.git";
+      defaultOptions = [
+        "--inline-info"
+        "--reverse"
+        "--height=30"
+        "--header-first"
+      ];
+    };
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    programs.bat.enable = true;
+    programs.eza.enable = true;
+    programs.brave.enable = true;
+    programs.zoxide.enable = true;
+    programs.gpg.enable = true;
+  };
 }
