bbfa77d0d9c43152965a7a61ebf41168b803d507
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-20 22:22:46 +0100

    dconf: remove missing seahorse/listing entry

--- dconf/settings.nix
+++ dconf/settings.nix
@@ -5,10 +5,6 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "apps/seahorse/listing" = {
-      keyrings-selected = [ "openssh:///home/callum/.ssh" ];
-    };
-
     "ca/desrt/dconf-editor" = {
       show-warning = false;
     };
