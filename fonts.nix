afac2ec924dfaa0b937780dd7fa5c8e8911a8291
Callum Dunster <cdunster@users.noreply.github.com>
2024-12-30 08:08:56 +0100

    nixos: move font packages to fonts.packages

--- fonts.nix
+++ fonts.nix
@@ -8,19 +8,16 @@ in
     (_final: prev: { cdunster = import inputs.cdunster-nixpkgs { system = prev.system; config = prev.config; }; })
   ];
 
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    cdunster.ttf-wps-fonts
+  ];
 
-  home-manager.users.${user} = {
-    home.packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-      cdunster.ttf-wps-fonts
-    ];
-
-    fonts.fontconfig = {
-      enable = true;
-      defaultFonts = {
-        sansSerif = [ "JetBrainsMono Nerd Font 10" ];
-        monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
-      };
+  home-manager.users.${user}.fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [ "JetBrainsMono Nerd Font 10" ];
+      monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
     };
   };
 }
