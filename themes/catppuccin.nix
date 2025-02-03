5cddf6d5a68200a972e11875d1bb5f67af0ba2d1
Callum Dunster <cdunster@users.noreply.github.com>
2025-02-03 15:21:10 +0100

    theme: rename tmux.catppuccin -> catppuccin.tmux

--- themes/catppuccin.nix
+++ themes/catppuccin.nix
@@ -30,6 +30,33 @@ in
         enable = true;
         gnomeShellTheme = cfg.desktopEnvironments.gnome.enable;
       };
+      tmux.extraConfig = ''
+        set -g @catppuccin_status_modules_left "session directory"
+        set -g @catppuccin_status_modules_right "cpu date_time"
+
+        set -g @catppuccin_window_status "icon"
+        set -g @catppuccin_icon_window_last ""
+        set -g @catppuccin_icon_window_current ""
+        set -g @catppuccin_window_left_separator " "
+        set -g @catppuccin_window_middle_separator " | "
+        set -g @catppuccin_window_right_separator ""
+        set -g @catppuccin_window_default_text "#W"
+        set -g @catppuccin_window_current_text "#W"
+
+        set -g @catppuccin_window_default_fill "none"
+        set -g @catppuccin_window_current_fill "all"
+        set -g @catppuccin_window_current_color "#{thm_blue}"
+        set -g @catppuccin_status_justify "absolute-centre"
+
+        set -g @catppuccin_directory_icon "󰉖"
+        set -g @catppuccin_session_icon ""
+
+        set -g @catppuccin_status_left_separator " "
+        set -g @catppuccin_status_right_separator ""
+        set -g @catppuccin_status_connect_separator "no"
+
+        set -g @catppuccin_status_background "#{thm_black}"
+      '';
     };
   };
 }
