c822b760b1ccdb8b5daf42a3c3720df5d6b60a19
Callum Dunster <cdunster@users.noreply.github.com>
2025-04-23 20:26:14 +0200

    dconf: always open wezterm as new process

--- dconf/settings.nix
+++ dconf/settings.nix
@@ -139,7 +139,7 @@ with lib.hm.gvariant;
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>t";
-      command = "wezterm";
+      command = "wezterm start --always-new-process";
       name = "Open WezTerm terminal";
     };
 
