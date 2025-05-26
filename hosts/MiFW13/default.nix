be6ccb93941913d9ae4f1aa1d3177db9f7ed2ec1
Callum Dunster <cdunster@users.noreply.github.com>
2025-05-26 09:06:41 +0200

    VM: swap VirtualBox with virt-manager

--- hosts/MiFW13/default.nix
+++ hosts/MiFW13/default.nix
@@ -5,9 +5,6 @@
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
 
-  # Workaround for "VirtualBox can't enable the AMD-V extension. Please disable the KVM kernel extension, recompile your kernel and reboot (VERR_SVM_IN_USE)."
-  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
-
   hostOptions = {
     userName = "Callum";
     allowUnfreePackages = true;
