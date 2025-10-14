35b6a02cbce43504d86b0b745f2b51f3c3e1d6d6
Callum Dunster <cdunster@users.noreply.github.com>
2025-10-14 12:36:07 +0200

    helix: add basic settings

--- helix.nix
+++ helix.nix
@@ -5,5 +5,27 @@ in
 {
   home-manager.users.${user}.programs.helix = {
     enable = true;
+    settings = {
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        trim-trailing-whitespace = true;
+        cursor-shape = {
+          insert = "bar";
+        };
+        whitespace = {
+          render = {
+            space = "all";
+            tab = "all";
+            nbsp = "none";
+            nnbsp = "none";
+            newline = "none";
+          };
+        };
+        indent-guides = {
+          render = true;
+        };
+      };
+    };
   };
 }
