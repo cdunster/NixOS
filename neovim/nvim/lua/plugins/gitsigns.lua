16c2fbb4138465623aa7e6da7fc5c7d639aa9d1c
Callum Dunster <cdunster@users.noreply.github.com>
2025-10-19 13:36:13 +0200

    nvim: replace " with ' for strings in gitsigns.lua file

--- neovim/nvim/lua/plugins/gitsigns.lua
+++ neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,40 +1,40 @@
 local M = {}
 
 M.config = function()
-    require("gitsigns").setup({
+    require('gitsigns').setup({
         current_line_blame = true,
     })
 
-    require("which-key").add({
-        { "]g", require('gitsigns.actions').next_hunk, desc = "Next git hunk" },
-        { "[g", require('gitsigns.actions').prev_hunk, desc = "Previous git hunk" },
+    require('which-key').add({
+        { ']g', require('gitsigns.actions').next_hunk, desc = 'Next git hunk' },
+        { '[g', require('gitsigns.actions').prev_hunk, desc = 'Previous git hunk' },
 
         {
-            { "<leader>g",  group = "+git" },
-            { "<leader>gs", require('gitsigns').stage_hunk,      desc = "Stage current hunk" },
-            { "<leader>gS", require('gitsigns').stage_buffer,    desc = "Stage current buffer" },
-            { "<leader>gu", require('gitsigns').undo_stage_hunk, desc = "Undo staging last hunk" },
-            { "<leader>gr", require('gitsigns').reset_hunk,      desc = "Reset current hunk" },
-            { "<leader>gR", require('gitsigns').reset_buffer,    desc = "Reset current buffer" },
-            { "<leader>gp", require('gitsigns').preview_hunk,    desc = "Preview current hunk" },
+            { '<leader>g',  group = '+git' },
+            { '<leader>gs', require('gitsigns').stage_hunk,      desc = 'Stage current hunk' },
+            { '<leader>gS', require('gitsigns').stage_buffer,    desc = 'Stage current buffer' },
+            { '<leader>gu', require('gitsigns').undo_stage_hunk, desc = 'Undo staging last hunk' },
+            { '<leader>gr', require('gitsigns').reset_hunk,      desc = 'Reset current hunk' },
+            { '<leader>gR', require('gitsigns').reset_buffer,    desc = 'Reset current buffer' },
+            { '<leader>gp', require('gitsigns').preview_hunk,    desc = 'Preview current hunk' },
         },
 
         {
-            mode = "o",
-            { "ih", require('gitsigns.actions').select_hunk, desc = "Select git hunk" },
+            mode = 'o',
+            { 'ih', require('gitsigns.actions').select_hunk, desc = 'Select git hunk' },
         },
 
         {
-            mode = "x",
-            { "ih", require('gitsigns.actions').select_hunk, desc = "Select git hunk" },
+            mode = 'x',
+            { 'ih', require('gitsigns.actions').select_hunk, desc = 'Select git hunk' },
         },
 
 
         {
-            mode = "v",
-            { "<leader>g",  group = "+git" },
-            { "<leader>gs", function() require('gitsigns').stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = "Stage selected hunk(s)" },
-            { "<leader>gr", function() require('gitsigns').reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = "Reset selected hunk(s)" },
+            mode = 'v',
+            { '<leader>g',  group = '+git' },
+            { '<leader>gs', function() require('gitsigns').stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = 'Stage selected hunk(s)' },
+            { '<leader>gr', function() require('gitsigns').reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = 'Reset selected hunk(s)' },
         },
     })
 end
