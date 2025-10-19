e5f8849032c3580b7ffce91f3de874596f440b1b
Callum Dunster <cdunster@users.noreply.github.com>
2025-10-19 13:37:46 +0200

    nvim: replace " with ' for strings in statusline.lua file

--- neovim/nvim/lua/plugins/statusline.lua
+++ neovim/nvim/lua/plugins/statusline.lua
@@ -1,12 +1,12 @@
 -- Set the custom statusline.
-vim.o.statusline = "[%n] %<%f%m %{v:lua.LspStatus()} %{ObsessionStatus()}%=%{FugitiveHead()}"
+vim.o.statusline = '[%n] %<%f%m %{v:lua.LspStatus()} %{ObsessionStatus()}%=%{FugitiveHead()}'
 
 function _G.LspStatus()
     if vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
-        return ""
+        return ''
     end
 
     local err = table.getn(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }))
     local warn = table.getn(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN }))
-    return "E"..tostring(err).." W"..tostring(warn)
+    return 'E' .. tostring(err) .. ' W' .. tostring(warn)
 end
