2570d1702b47946e01c611958904e0918d98b1b7
Callum Dunster <cdunster@users.noreply.github.com>
2026-01-05 14:53:36 +0100

    overseer: update config after update

--- neovim/nvim/lua/plugins/overseer.lua
+++ neovim/nvim/lua/plugins/overseer.lua
@@ -4,12 +4,11 @@ M.config = function()
     local overseer = require('overseer')
 
     overseer.setup()
-    overseer.load_template("nix")
 
     require('which-key').add({
         { '<leader>o',  group = '+overseer' },
-        { '<leader>or', require('overseer').run_template, desc = 'Show list of runnable tasks' },
-        { '<leader>oo', require('overseer').open,         desc = 'Open list of running or ran tasks' },
+        { '<leader>or', require('overseer').run_task, desc = 'Show list of runnable tasks' },
+        { '<leader>oo', require('overseer').open,     desc = 'Open list of running or ran tasks' },
     })
 end
 
