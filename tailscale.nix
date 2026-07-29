85b594adc858df22495ff038963a90b1cd9e9670
Callum Dunster <cdunster@users.noreply.github.com>
2026-07-29 11:48:11 +0200

    tailscale: use nftables to set networking rules instead of iptables

--- tailscale.nix
+++ tailscale.nix
@@ -21,6 +21,25 @@ with lib;
         useRoutingFeatures = "client";
       };
 
+      # Enable the firewall using nftables
+      networking.nftables.enable = true;
+      networking.firewall = {
+        enable = true;
+        # Always allow traffic from your Tailscale network
+        trustedInterfaces = [ config.services.tailscale.interfaceName ];
+        # Allow the Tailscale UDP port through the firewall
+        allowedUDPPorts = [ config.services.tailscale.port ];
+      };
+
+      # Force tailscaled to use nftables
+      systemd.services.tailscaled.serviceConfig.Environment = [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
+      ];
+
+      # Prevent systemd from waiting for network online
+      systemd.network.wait-online.enable = false;
+      boot.initrd.systemd.network.wait-online.enable = false;
+
       home-manager.users.${user} = {
         programs.gnome-shell = mkIf gnomeEnabled {
           extensions = with pkgs.gnomeExtensions; [
