02662e98294cfc1f1ff2433cef62f482e4e6eb96
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-04 23:23:38 +0200

    nixos: move time and locale into separate module

--- nixos/default.nix
+++ nixos/default.nix
@@ -18,6 +18,7 @@
     ./tailscale.nix
     ./terminals
     ./themes
+    ./time-and-locale.nix
     ./udev
     ./users.nix
   ];
@@ -59,9 +60,6 @@
         };
       };
 
-      # Set the system timezone.
-      time.timeZone = lib.mkDefault "Europe/Amsterdam";
-
       # Manage networks with networkmanager.
       networking.networkmanager = {
         enable = true;
@@ -70,25 +68,6 @@
         plugins = with pkgs; [
           networkmanager-openvpn
         ];
-      };
-
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_GB.UTF-8";
-
-      i18n.extraLocaleSettings = {
-        LC_CTYPE = "en_GB.UTF-8";
-        LC_NUMERIC = "en_GB.UTF-8";
-        LC_TIME = "en_GB.UTF-8";
-        LC_COLLATE = "en_GB.UTF-8";
-        LC_MONETARY = "en_GB.UTF-8";
-        LC_MESSAGES = "en_GB.UTF-8";
-        LC_PAPER = "en_GB.UTF-8";
-        LC_NAME = "en_GB.UTF-8";
-        LC_ADDRESS = "en_GB.UTF-8";
-        LC_TELEPHONE = "en_GB.UTF-8";
-        LC_MEASUREMENT = "en_GB.UTF-8";
-        LC_IDENTIFICATION = "en_GB.UTF-8";
-        LC_ALL = "en_GB.UTF-8";
       };
 
       # Override default packages (removes nano and others).
