bdd157bf2426c856fd2533f8bb804e4bafad317d
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-07 10:34:50 +0200

    autorandr: remove package completely
    I'm on Wayland for the forseeable future it seems.

--- nixos/desktop-envs/default.nix
+++ nixos/desktop-envs/default.nix
@@ -1,7 +1,6 @@
 { ... }: {
   imports = [
     # Utilities and tooling for multiple DM/DE/WM combinations
-    ./utils/autorandr.nix # Automatically configure displays for X11
     ./utils/gui-scale-applet.nix # Tailscale management applet for the COSMIC panel
 
     ./gnome.nix # The GNOME Desktop Environment
