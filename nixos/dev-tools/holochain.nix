2c848e6e54a92acd46a6ab5622c7f6a491056359
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-07 14:23:32 +0200

    holochain: remove colmena as it is added via devShells when required

--- nixos/dev-tools/holochain.nix
+++ nixos/dev-tools/holochain.nix
@@ -33,7 +33,6 @@
         mattermost-desktop # Desktop client Mattermost; a collaboration and chat app for businesses
         zoom-us # Video conferencing software
         discord # Chat desktop app for gamers and communities
-        colmena # Simple and stateless NixOS deployment tool
       ];
     };
 }
