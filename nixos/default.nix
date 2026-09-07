04012bb92a6a32d7b214b7a549679a903dfb554d
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-07 13:31:41 +0200

    fonts: move config into correct modules

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,7 +6,6 @@
     ./dev-tools
     ./display-managers
     ./docker.nix
-    ./fonts.nix
     ./git.nix
     ./gpg.nix
     ./helix.nix
@@ -53,5 +52,10 @@
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "23.11"; # Did you read the comment?
+
+    # Install extra system fonts
+    fonts.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+    ];
   };
 }
--- users/callum/home-manager/default.nix
+++ users/callum/home-manager/default.nix
@@ -7,5 +7,14 @@
   config = {
     # Add user profile picture.
     home.file.".face".source = ../../../images/backgrounds/deer_sunset.jpg;
+
+    # Set user's fonts, need to be installed on the system
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "JetBrainsMono Nerd Font 10" ];
+        monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
+      };
+    };
   };
 }
