7f5fbc545d69971ea8be4d7dd8f0f189db6bdb08
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-07 14:16:20 +0200

    fonts: move into the style module

--- users/callum/home-manager/default.nix
+++ users/callum/home-manager/default.nix
@@ -12,15 +12,6 @@
     # Add user profile picture.
     home.file.".face".source = ../../../images/backgrounds/deer_sunset.jpg;
 
-    # Set user's fonts, need to be installed on the system
-    fonts.fontconfig = {
-      enable = true;
-      defaultFonts = {
-        sansSerif = [ "JetBrainsMono Nerd Font 10" ];
-        monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
-      };
-    };
-
     # User-specific git settings
     programs.git.settings.user = {
       name = "Callum Dunster";
--- users/callum/home-manager/style/default.nix
+++ users/callum/home-manager/style/default.nix
@@ -4,6 +4,15 @@
   ];
 
   config = {
+    # Set user's fonts, need to be installed on the system
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "JetBrainsMono Nerd Font 10" ];
+        monospace = [ "JetBrainsMono Nerd Font Mono 11" ];
+      };
+    };
+
     gtk = {
       enable = true;
       gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
