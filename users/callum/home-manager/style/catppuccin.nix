09cc84e40c1054400ccbc9c39fd07b21610bab2d
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-08 18:06:53 +0200

    tmux: remove completely

--- users/callum/home-manager/style/catppuccin.nix
+++ users/callum/home-manager/style/catppuccin.nix
@@ -1,8 +1,4 @@
-{ inputs, lib, config, osConfig, ... }:
-let
-  tmuxEnabled = config.programs.tmux.enable;
-in
-{
+{ inputs, osConfig, ... }: {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
   ];
@@ -11,33 +7,6 @@ in
     catppuccin = {
       inherit (osConfig.catppuccin) enable autoEnable flavor accent;
       cursors.accent = "dark";
-      tmux.extraConfig = lib.mkIf tmuxEnabled ''
-        set -g @catppuccin_status_modules_left "session directory"
-        set -g @catppuccin_status_modules_right "cpu date_time"
-
-        set -g @catppuccin_window_status "icon"
-        set -g @catppuccin_icon_window_last ""
-        set -g @catppuccin_icon_window_current ""
-        set -g @catppuccin_window_left_separator " "
-        set -g @catppuccin_window_middle_separator " | "
-        set -g @catppuccin_window_right_separator ""
-        set -g @catppuccin_window_default_text "#W"
-        set -g @catppuccin_window_current_text "#W"
-
-        set -g @catppuccin_window_default_fill "none"
-        set -g @catppuccin_window_current_fill "all"
-        set -g @catppuccin_window_current_color "#{thm_blue}"
-        set -g @catppuccin_status_justify "absolute-centre"
-
-        set -g @catppuccin_directory_icon "󰉖"
-        set -g @catppuccin_session_icon ""
-
-        set -g @catppuccin_status_left_separator " "
-        set -g @catppuccin_status_right_separator ""
-        set -g @catppuccin_status_connect_separator "no"
-
-        set -g @catppuccin_status_background "#{thm_black}"
-      '';
     };
   };
 }
--- users/callum/home-manager/terminals/default.nix
+++ users/callum/home-manager/terminals/default.nix
@@ -1,7 +1,6 @@
 { ... }: {
   imports = [
     # Utilities and tooling for multiple terminals
-    ./utils/tmux.nix
     ./utils/zellij.nix
 
     ./kitty.nix
