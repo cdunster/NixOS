6357df78f77cb95779c363a7219afebadadfeecd
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-08 20:03:53 +0200

    style: fix stylix/COSMIC conflict

--- home-manager-shared/cosmic-de/default.nix
+++ home-manager-shared/cosmic-de/default.nix
@@ -1,6 +1,14 @@
-{ ... }: {
+{ lib, osConfig, ... }: {
   imports = [
     # An applet to control Tailscale networks and settings
     ./gui-scale-applet.nix
   ];
+
+  config = {
+    # If COSMIC DE is enabled, let it manage GTK and Qt theming instead of Stylix
+    stylix.targets = lib.mkIf osConfig.services.desktopManager.cosmic.enable {
+      gtk.enable = false;
+      qt.enable = false;
+    };
+  };
 }
