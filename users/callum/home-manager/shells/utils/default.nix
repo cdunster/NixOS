7ec88e9f19893a9fc01cea21ed958588c1f4edeb
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-09 12:03:44 +0200

    starship: use stylix for the prompt colours

--- users/callum/home-manager/shells/utils/default.nix
+++ users/callum/home-manager/shells/utils/default.nix
@@ -1,23 +1,15 @@
-{ ... }: {
+{ config, ... }: {
   programs = {
     # The custom, cross-shell shell prompt
     starship = {
       enable = true;
       settings = {
-        aws.style = "bold #ffb86c";
-        cmd_duration.style = "bold #f1fa8c";
-        directory.style = "bold #50fa7b";
-        hostname.style = "bold #ff5555";
-        git_branch.style = "bold #ff79c6";
-        git_status.style = "bold #ff5555";
-        username = {
-          format = "[$user]($style) on ";
-          style_user = "bold #bd93f9";
-        };
+        # Display the current shell in the prompt
         shell.disabled = false;
+        # Set the character used on the input line of the prompt
         character = {
-          success_symbol = "[⇒](bold #f8f8f2)";
-          error_symbol = "[⇏](bold #ff5555)";
+          success_symbol = "[⇒](bold #${config.lib.stylix.colors.base05})";
+          error_symbol = "[⇏](bold #${config.lib.stylix.colors.red})";
         };
       };
     };
