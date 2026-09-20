3dea8c5689fd6c69c1eb6657a17cda623f3b9c75
Callum Dunster <cdunster@users.noreply.github.com>
2026-09-20 12:29:35 +0200

    nix: set nixos-hardware nixpkgs inputs to follow system

--- flake.nix
+++ flake.nix
@@ -21,7 +21,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { ... }@inputs:
